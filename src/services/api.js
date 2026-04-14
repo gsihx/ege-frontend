e9483e9186fc945345df8a82628506c7a2c82742
Kirill Burdin <burdin_109@mail.ru>
2026-04-14 23:16:30 +0700

    тест консоли

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 1. Создаем базовый экземпляр Axios
 const api = axios.create({
-  baseURL: 'https://backend-production-bf52.up.railway.app' 
+  baseURL: 'https://ege-api2-gsihx.amvera.io'
 });
 
 // 2. ПЕРЕХВАТЧИК ЗАПРОСОВ (Request Interceptor)
@@ -11,12 +11,12 @@ api.interceptors.request.use(
   (config) => {
     // Пытаемся достать токен из памяти браузера
     const token = localStorage.getItem('token');
-    
+
     // Если токен есть, приклеиваем его к заголовку Authorization
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-    
+
     return config;
   },
   (error) => {
@@ -35,14 +35,14 @@ api.interceptors.response.use(
     // Если сервер ответил ошибкой 401 (Токен истек, неверный или отсутствует)
     if (error.response && error.response.status === 401) {
       console.warn('Токен истек или недействителен. Выполняем выход...');
-      
+
       // Очищаем старые данные сессии
       localStorage.removeItem('token');
       localStorage.removeItem('user_id');
       localStorage.removeItem('username');
-      
+
       // Перенаправляем пользователя на страницу входа
-      window.location.href = '/login'; 
+      window.location.href = '/login';
     }
     return Promise.reject(error);
   }
